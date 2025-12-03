195242c95d3c7431a8ccba578376b819214fa4b0
davnotdev <usr21p1@gmail.com>
2025-12-02 16:44:39 -0800

    Fix typo and bump to 0.1.1

--- src/correction.rs
+++ src/correction.rs
@@ -5,9 +5,9 @@ use super::*;
 pub enum CorrectionType {
     /// A combined image sampler has been split, a new `sampler` object should be inserted.
     SplitCombined = 0,
-    /// A mixed depth texture / sampler has been split, insert the same object again with a `Regular` bind type.
+    /// A mixed depth texture / sampler has been duplicated, insert the same object again with a `Regular` bind type.
     SplitDrefRegular = 1,
-    /// A mixed depth texture / sampler has been split, insert the same object again with a
+    /// A mixed depth texture / sampler has been duplicated, insert the same object again with a
     /// `Comparison` bind type.
     SplitDrefComparison = 2,
 }
